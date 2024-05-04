4f4c51c2e1df54664c647ef74de76b251a68196d
Luiz Gustavo Marques <62360761+cmarquesluizgustavo@users.noreply.github.com>
2024-05-04 11:20:48 -0300

    after changes in design

--- components/statsChart/index.tsx
+++ components/statsChart/index.tsx
@@ -5,125 +5,128 @@ import type { Layout, PlotData, PlotType } from 'plotly.js'
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false })
 
 export default function statsChart(stats: IStats[]) {
-	const statsByLabelNetworkType = stats.reduce((acc, stat) => {
-		let title = `${stat.label}-${
-			stat.network_id < 1000 ? 'legislature' : 'year'
-		}`
+    const statsByLabelNetworkType = stats.reduce((acc, stat) => {
+        let title = `${stat.label}-${
+            stat.network_id < 1000 ? 'legislature' : 'year'
+        }`
 
-		if (!acc[title]) {
-			acc[title] = []
-		}
+        if (!acc[title]) {
+            acc[title] = []
+        }
 
-		acc[title].push(stat)
+        acc[title].push(stat)
 
-		return acc
-	}, {} as Record<string, IStats[]>)
+        return acc
+    }, {} as Record<string, IStats[]>)
 
-	const minY = Math.min(...stats.map((stat) => stat.value))
-	const maxY = Math.max(...stats.map((stat) => stat.value))
+    const minY = Math.min(...stats.map((stat) => stat.value))
+    const maxY = Math.max(...stats.map((stat) => stat.value))
 
-	const statsByLabelYear = Object.entries(statsByLabelNetworkType).reduce(
-		(acc, [key, value]) => {
-			const [label, networkType] = key.split('-')
+    const statsByLabelYear = Object.entries(statsByLabelNetworkType).reduce(
+        (acc, [key, value]) => {
+            const [label, networkType] = key.split('-')
 
-			if (!acc[label]) {
-				acc[label] = []
-			}
+            if (!acc[label]) {
+                acc[label] = []
+            }
 
-			if (networkType === 'year') {
-				acc[label].push(...value)
-			}
+            if (networkType === 'year') {
+                acc[label].push(...value)
+            }
 
-			return acc
-		},
-		{} as Record<string, IStats[]>
-	)
+            return acc
+        },
+        {} as Record<string, IStats[]>
+    )
 
-	const statsByLabelLegislature = Object.entries(
-		statsByLabelNetworkType
-	).reduce((acc, [key, value]) => {
-		const [label, networkType] = key.split('-')
+    const statsByLabelLegislature = Object.entries(
+        statsByLabelNetworkType
+    ).reduce((acc, [key, value]) => {
+        const [label, networkType] = key.split('-')
 
-		if (!acc[label]) {
-			acc[label] = []
-		}
+        if (!acc[label]) {
+            acc[label] = []
+        }
 
-		if (networkType === 'legislature') {
-			acc[label].push(...value)
-		}
+        if (networkType === 'legislature') {
+            acc[label].push(...value)
+        }
 
-		return acc
-	}, {} as Record<string, IStats[]>)
+        return acc
+    }, {} as Record<string, IStats[]>)
 
-	const minXYear = Math.min(
-		...Object.values(statsByLabelYear)
-			.flat()
-			.map((stats) => stats.network_id)
-	)
-	const maxXYear = Math.max(
-		...Object.values(statsByLabelYear)
-			.flat()
-			.map((stats) => stats.network_id)
-	)
+    const minXYear = Math.min(
+        ...Object.values(statsByLabelYear)
+            .flat()
+            .map((stats) => stats.network_id)
+    )
+    const maxXYear = Math.max(
+        ...Object.values(statsByLabelYear)
+            .flat()
+            .map((stats) => stats.network_id)
+    )
 
-	const minXLegislature = Math.min(
-		...Object.values(statsByLabelLegislature)
-			.flat()
-			.map((stats) => stats.network_id)
-	)
+    const minXLegislature = Math.min(
+        ...Object.values(statsByLabelLegislature)
+            .flat()
+            .map((stats) => stats.network_id)
+    )
 
-	const maxXLegislature = Math.max(
-		...Object.values(statsByLabelLegislature)
-			.flat()
-			.map((stats) => stats.network_id)
-	)
+    const maxXLegislature = Math.max(
+        ...Object.values(statsByLabelLegislature)
+            .flat()
+            .map((stats) => stats.network_id)
+    )
 
-	const layout: Partial<Layout> = {
-		xaxis: {
-			title: 'Year',
-			range: [minXYear, maxXYear]
-		},
-		xaxis2: {
-			title: 'Legislature',
-			range: [minXLegislature, maxXLegislature],
-			overlaying: 'x',
-			side: 'top'
-			// type: 'category'
-		},
-		yaxis: {
-			title: 'Similarity Index',
-			range: [minY, maxY]
-		},
-		yaxis2: {
-			// title: 'Per Legislature',
-			range: [minY, maxY],
-			overlaying: 'y',
-			side: 'right'
-		}
-		// title: 'Congress Person Stats'
-	}
+    const layoutYear: Partial<Layout> = {
+        xaxis: {
+            title: 'Year',
+            range: [minXYear, maxXYear]
+        },
+        yaxis: {
+            title: 'Similarity Gain',
+            range: [minY, maxY]
+        }
+    }
 
-	const data: Partial<PlotData>[] = [
-		...Object.entries(statsByLabelYear).map(([label, stats]) => ({
-			x: stats.map((stat) => stat.network_id).sort(),
-			y: stats.map((stat) => stat.value).sort(),
-			type: 'scatter' as PlotType,
-			mode: 'lines+markers' as const,
-			name: label,
-			line: { color: label }
-		})),
-		...Object.entries(statsByLabelLegislature).map(([label, stats]) => ({
-			x: stats.map((stat) => stat.network_id).sort(),
-			y: stats.map((stat) => stat.value).sort(),
-			type: 'scatter' as PlotType,
-			mode: 'lines+markers' as const,
-			name: label,
-			xaxis: 'x2',
-			yaxis: 'y2',
-			line: { dash: 'dash', color: label },
-			marker: { symbol: 'star' }
-		}))
-	].sort((a, b) => a.name.localeCompare(b.name))
+    const layoutLegislature: Partial<Layout> = {
+        xaxis: {
+            title: 'Legislature',
+            range: [minXLegislature, maxXLegislature]
+        },
+        yaxis: {
+            title: 'Similarity Gain',
+            range: [minY, maxY]
+        }
+    }
 
-	return (<div className='flex'><Plot data={data} layout={layout} /></div>)
+    const dataYear: Partial<PlotData>[] = Object.entries(statsByLabelYear).map(([label, stats]) => ({
+        x: stats.map((stat) => stat.network_id).sort(),
+        y: stats.map((stat) => stat.value).sort(),
+        type: 'scatter' as PlotType,
+        mode: 'lines+markers' as const,
+        name: label,
+        line: { color: label }
+    }))
+
+    const dataLegislature: Partial<PlotData>[] = Object.entries(statsByLabelLegislature).map(([label, stats]) => ({
+        x: stats.map((stat) => stat.network_id).sort(),
+        y: stats.map((stat) => stat.value).sort(),
+        type: 'scatter' as PlotType,
+        mode: 'lines+markers' as const,
+        name: label,
+        line: { color: label },
+        marker: { symbol: 'dot', size: 5}
+    }))
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>
+                <Plot data={dataYear} layout={layoutYear} />
+            </div>
+            <div>
+                <Plot data={dataLegislature} layout={layoutLegislature} />
+            </div>
+        </div>
+    )
 }
--- pages/congress_person_over_time/index.tsx
+++ pages/congress_person_over_time/index.tsx
@@ -10,189 +10,205 @@ import type { NextPage } from 'next'
 
 import { ReactElement, useEffect, useState } from 'react'
 
-
-
 interface CongressMember extends Record<string, string | number> {
-	id: number
-	name: string
-	birth_state: string
-	education: string
-	birth_date: string
-	sex: string
-	cpf: string
-	social_network: string
-	occupation: string
-	ethnicity: string
+  id: number
+  name: string
+  birth_state: string
+  education: string
+  birth_date: string
+  sex: string
+  cpf: string
+  social_network: string
+  occupation: string
+  ethnicity: string
 }
 
 async function getCongressMembers(): Promise<{ id: number; label: string }[]> {
-	console.log('Fetching congress members')
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}/congressperson`
-	)
+  console.log('Fetching congress members')
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/congressperson`
+  )
 
-	if (response.ok) {
-		const data = (await response.json()) as {
-			members: { id: number; name: string }[]
-		}
-		console.log(data)
+  if (response.ok) {
+    const data = (await response.json()) as {
+      members: { id: number; name: string }[]
+    }
+    console.log(data)
 
-		return data.members.map(({ id, name }) => ({ id, label: name }))
-	}
+    return data.members.map(({ id, name }) => ({ id, label: name }))
+  }
 
-	return []
+  return []
 }
 
 async function getCongressMember(id: number): Promise<CongressMember> {
-	console.log('Fetching congress member')
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}/congressperson/${id}`
-	)
+  console.log('Fetching congress member')
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/congressperson/${id}`
+  )
 
-	if (response.ok) {
-		const data = await response.json()
-		console.log(data)
+  if (response.ok) {
+    const data = await response.json()
+    console.log(data)
 
-		return data
-	}
+    return data
+  }
 
-	throw new Error('Failed to fetch congress member')
+  throw new Error('Failed to fetch congress member')
 }
 
 async function getCongresspersonStats(
-	congressperson_id: number
+  congressperson_id: number
 ): Promise<IStats[]> {
-	console.log('Fetching congressperson stats')
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}/stats/congressperson/${congressperson_id}`
-	)
+  console.log('Fetching congressperson stats')
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/stats/congressperson/${congressperson_id}`
+  )
 
-	if (response.ok) {
-		const data = (await response.json()) as {
-			stats: IStats[]
-		}
-		console.log(data)
+  if (response.ok) {
+    const data = (await response.json()) as {
+      stats: IStats[]
+    }
+    console.log(data)
 
-		return data.stats
-	}
+    return data.stats
+  }
 
-	return []
+  return []
 }
 
 const Page: NextPage = () => {
-	const [congressMembers, setCongressMembers] = useState<
-		{ id: number; label: string }[]
-	>([])
+  const [congressMembers, setCongressMembers] = useState<
+    { id: number; label: string }[]
+  >([])
 
-	const [congressMember, setCongressMember] = useState<CongressMember | null>(
-		null
-	)
+  const [congressMember, setCongressMember] = useState<CongressMember | null>(
+    null
+  )
 
-	const [congressStats, setCongressStats] = useState<IStats[]>([])
+  const [congressStats, setCongressStats] = useState<IStats[]>([])
 
-	const [algorithm, setAlgorithm] = useState<string>(algorithms[0])
+  const [algorithm, setAlgorithm] = useState<string>(algorithms[0])
 
-	useEffect(() => {
-		try {
-			const members = getCongressMembers().then((members) => {
-				setCongressMembers(members)
-			})
-		} catch (error) {
-			console.error(error)
-			alert('Failed to fetch congress members')
-			setCongressMembers([])
-		}
-	}, [])
+  useEffect(() => {
+    try {
+      const members = getCongressMembers().then((members) => {
+        setCongressMembers(members)
+      })
+    } catch (error) {
+      console.error(error)
+      alert('Failed to fetch congress members')
+      setCongressMembers([])
+    }
+  }, [])
 
-	const congressMemberOnChange = async (
-		_: any,
-		value: { label: string; id: number } | null
-	) => {
-		console.log(`selecionei`, { value })
+  const congressMemberOnChange = async (
+    _: any,
+    value: { label: string; id: number } | null
+  ) => {
+    console.log(`selecionei`, { value })
 
-		if (!value) {
-			setCongressMember(null)
-			setCongressStats([])
-			return
-		}
+    if (!value) {
+      setCongressMember(null)
+      setCongressStats([])
+      return
+    }
 
-		try {
-			const [member, stats] = await Promise.all([
-				getCongressMember(value.id),
-				getCongresspersonStats(value.id)
-			])
-			setCongressMember(member)
-			setCongressStats(stats)
-		} catch (error) {
-			console.error(error)
-			alert('Failed to fetch congress member')
-			setCongressMember(null)
-			setCongressStats([])
-		}
-	}
+    try {
+      const [member, stats] = await Promise.all([
+        getCongressMember(value.id),
+        getCongresspersonStats(value.id)
+      ])
+      setCongressMember(member)
+      setCongressStats(stats)
+    } catch (error) {
+      console.error(error)
+      alert('Failed to fetch congress member')
+      setCongressMember(null)
+      setCongressStats([])
+    }
+  }
 
-	const algorithmOnChange = async (
-		value: { label: string; id: string } | null
-	) => {
-		console.log('Mutating algorithm', { value })
-		setAlgorithm(!value ? algorithms[0] : value.id)
-	}
+  const algorithmOnChange = async (
+    value: { label: string; id: string } | null
+  ) => {
+    console.log('Mutating algorithm', { value })
+    setAlgorithm(!value ? algorithms[0] : value.id)
+  }
 
-	const statsToShow = congressStats.filter((stat) => stat.type === algorithm)
+  const statsToShow = congressStats.filter((stat) => stat.type === algorithm)
 
-	return (
-		<Container>		
-			<h1 className="text-5xl font-bold">Congress Person Over Time</h1>
-			<div className="flex gap-4 mt-8">
-				<Autocomplete
-					disablePortal
-					id="combo-box-demo"
-					options={congressMembers}
-					sx={{ width: 300 }}
-					renderInput={(params) => (
-						<TextField {...params} label="Congressistas"  />
-					)}
-					onChange={(event, value) =>
-						congressMemberOnChange(event, value)
-					}
-					className="flex"
-				/>
-				<FormControl fullWidth>
-					<InputLabel id="demo-simple-select-label">
-						Algoritmo
-					</InputLabel>
-					<Select
-						labelId="demo-simple-select-label"
-						id="demo-simple-select"
-						value={algorithm}
-						label="Age"
-						onChange={(event, value) => {
-							const props = (value as ReactElement)?.props as {
-								children: string
-								value: string
-							}
+  return (
+    <Container>
+      <h1 className="text-5xl font-bold">Congressperson Over Time</h1>
+      <div className="flex gap-4 mt-8">
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={congressMembers}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Congressistas" />
+          )}
+          onChange={(event, value) =>
+            congressMemberOnChange(event, value)
+          }
+          className="flex"
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Algoritmo</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={algorithm}
+            label="Age"
+            onChange={(event, value) => {
+              const props = (value as ReactElement)?.props as {
+                children: string
+                value: string
+              }
 
-							algorithmOnChange(
-								props && {
-									label: props.children,
-									id: props.value
-								}
-							)
-						}}
-					>
-						{algorithms.map((algorithm) => (
-							<MenuItem key={algorithm} value={algorithm}>
-								{algorithm}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</div>
-			{congressMember &&
-				congressMemberData(congressMember, 'Congress Member Data')}
-			{statsToShow.length > 0 && congressStatsChart(statsToShow)}
-		</Container>
-	)
+              algorithmOnChange(
+                props && {
+                  label: props.children,
+                  id: props.value
+                }
+              )
+            }}
+          >
+            {algorithms.map((algorithm) => (
+              <MenuItem key={algorithm} value={algorithm}>
+                {algorithm}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="flex mt-8">
+        {congressMember && (
+          <div className="flex-1 ml-8">
+            <h2 className="text-3xl font-bold">Congress Member Data</h2>
+            <table className="table-auto mt-4">
+              <tbody>
+                {Object.entries(congressMember).map(([key, value]) => (
+                  <tr key={key}>
+                    <td className="border px-4 py-2">{key}</td>
+                    <td className="border px-4 py-2">{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {statsToShow.length > 0 && (
+  			<div className="flex-1 ml-8">
+    			<h2 className="text-3xl font-bold">Influences gain over time</h2>
+    				{congressStatsChart(statsToShow)}
+  			</div>
+		)}
+      </div>
+    </Container>
+  )
 }
 
 export default Page
